a110f9bd9afa18ed55c13cd3f4688bc8a57efbb0
Kennethso <kennethso1211@gmail.com>
2022-04-27 18:24:06 +0800

    Basic, _1, _2

--- Python/lv_1_basic/_1_variables.py
+++ Python/lv_1_basic/_1_variables.py
@@ -1,12 +1,8 @@
-import json
-class Person:
-    def __init__(self, name, age, sex):
-        self.name = name
-        self.age = age
-        self.sex = sex
-
-person = Person("Ken",5,'M')
-person2 = person
-person2.sex = 'F'
-print(json.dumps(person.sex))
-print(json.dumps(person2.sex))
+name = 'Kenneth'
+age = 30
+sex = 'M'
+is_married = True
+print(name)
+print(age)
+print(sex)
+print(is_married)
---
+++ Python/lv_1_basic/_2_conditions.py
@@ -0,0 +1,24 @@
+# <
+print(3 < 2) # False 
+print(2 < 2) # False 
+print(2 < 3) # True
+# <= 
+print(3 <= 2) # False 
+print(2 <= 2) # True 
+print(2 <= 3) # True
+# > 
+print(3 > 2) # True 
+print(2 > 2) # False 
+print(2 > 3) # False
+# >= 
+print(3 >= 2) # True 
+print(2 >= 2) # True
+print(2 >= 3) # False
+# == 
+print(3 == 2) # False 
+print(2 == 2) # True
+print(2 == 3) # False
+# !=  
+print(3 != 2) # True 
+print(2 != 2) # False 
+print(2 != 3) # True
